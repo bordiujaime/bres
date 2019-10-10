7a09d0e5cbe0faae7ce5f3f152c60a4e480aaae9
jaime Bordiu <bordiujaime@gmail.com>
2019-10-10 20:47:53 +0200

    db url

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ require("./config/passport")
 
 
 mongoose
-  .connect('mongodb://localhost/bres', { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI, { useNewUrlParser: true })
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
